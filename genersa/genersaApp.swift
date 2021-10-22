fbd1a97f3e9797878c073ef39c4d4ef5c8025bc3
leonardo <leonardo.14117125@student.itera.ac.id>
2021-10-22 16:24:57 +0700

    Initial Commit

---
+++ genersa/genersaApp.swift
@@ -0,0 +1,17 @@
+//
+//  genersaApp.swift
+//  genersa
+//
+//  Created by Leo nardo on 22/10/21.
+//
+
+import SwiftUI
+
+@main
+struct genersaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
