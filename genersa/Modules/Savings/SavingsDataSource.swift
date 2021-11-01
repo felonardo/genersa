184daa0c20c05fec56c1bc316cc884aef78ab6ea
Kevin Rivaldo <kevinrivaldo12.kr@gmail.com>
2021-11-01 08:58:42 +0700

    SavingsDataSource

---
+++ genersa/Modules/Savings/SavingsDataSource.swift
@@ -0,0 +1,11 @@
+//
+//  SavingsDataSource.swift
+//  genersa
+//
+//  Created by Kevin Rivaldo  on 01/11/21.
+//
+
+import Foundation
+import CoreData
+
+
---
+++ genersa/Modules/Savings/SavingsModel.swift
@@ -0,0 +1,11 @@
+//
+//  SavingsModel.swift
+//  genersa
+//
+//  Created by Kevin Rivaldo  on 01/11/21.
+//
+
+import Foundation
+import CoreData
+
+
