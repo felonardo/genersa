ee11a442cf10a140d5fd374fbea6207459a94e13
leonardo <leonardo.14117125@student.itera.ac.id>
2021-11-01 16:54:58 +0700

    Sprint 1 -Review and Refactoring

--- genersa/Modules/Trip/Model/TripDataSource.swift
+++ genersa/Modules/Trip/Model/TripDataSource.swift
@@ -9,58 +9,75 @@ import Foundation
 import CoreData
 
 class TripDataSource {
+    
     static let shared = TripDataSource()
     var trips: [Trip] = []
     var container: NSPersistentCloudKitContainer
+    
     init() {
         container = PersistenceController.shared.container
     }
+    
     func getTrip(with id: UUID) -> Trip? {
         return trips.first(where: {$0.id == id})
     }
-    func createTrip(startDate: Date, endDate: Date,
+    
+    func createTrip(currency: String, startDate: Date, endDate: Date,
                     name: String, totalBudget: Double, settlement: Settlement,
-                    debts: [Debt], users: [User], budgets: [Budget]) {
+                    saving: Saving, debts: [Debt], users: [User], budgets: [Budget]) -> Trip {
+        
         let newTrip = Trip(context: container.viewContext)
         newTrip.id = UUID()
-//        newTrip.currency = currency
+        newTrip.currency = currency
         newTrip.startDate = startDate
         newTrip.endDate = endDate
         newTrip.name = name
         newTrip.totalBudget = totalBudget
         newTrip.settlements = settlement
-        // test
+        newTrip.savings = saving
+        newTrip.debts = NSSet(objects: debts)
         newTrip.debts?.addingObjects(from: debts)
+        newTrip.users = NSSet(objects: users)
         newTrip.users?.addingObjects(from: users)
+        newTrip.budgets = NSSet(objects: budgets)
         newTrip.budgets?.addingObjects(from: budgets)
+        trips.append(newTrip)
         PersistenceController.shared.save()
-        readTrips()
+        return newTrip
     }
-    func readTrips() {
+    func readTrips() -> Bool {
+        
         let request = Trip.fetchRequest()
         do {
             trips =  try container.viewContext.fetch(request)
+            return true
         } catch {
             print("Error reading trips. \(error.localizedDescription)")
+            return false
         }
     }
-    func updateTrip(id: UUID, name: String? = nil, totalBudget: Double? = nil) {
+    func updateTrip(id: UUID, name: String? = nil, totalBudget: Double? = nil) -> Bool {
+        
         if let trip = getTrip(with: id) {
             trip.name = name ?? trip.name
             trip.totalBudget = totalBudget ?? trip.totalBudget
             PersistenceController.shared.save()
-            readTrips()
+            return true
         } else {
             print("Update trip failed. Trip with id (\(id)), not found")
+            return false
         }
     }
-    func deleteTrip(id: UUID) {
+    func deleteTrip(id: UUID) -> Bool {
+        
         if let trip = getTrip(with: id) {
             container.viewContext.delete(trip)
+            trips.removeAll(where: {$0.id == id})
             PersistenceController.shared.save()
-            readTrips()
+            return true
         } else {
             print("Delete trip failed. Trip with id (\(id)), not found")
+            return false
         }
     }
 }
