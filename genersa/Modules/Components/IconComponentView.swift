70308c67c77e51d1d627ac0e4fbb2a8095604b1b
Kevin Rivaldo <kevinrivaldo12.kr@gmail.com>
2021-11-05 16:27:51 +0700

    IconComponentView

---
+++ genersa/Modules/Components/IconComponentView.swift
@@ -0,0 +1,20 @@
+//
+//  IconComponentView.swift
+//  genersa
+//
+//  Created by Kevin Rivaldo  on 04/11/21.
+//
+
+import SwiftUI
+
+struct IconComponentView: View {
+    var body: some View {
+        Circle()
+    }
+}
+
+struct IconComponentView_Previews: PreviewProvider {
+    static var previews: some View {
+        IconComponentView()
+    }
+}
