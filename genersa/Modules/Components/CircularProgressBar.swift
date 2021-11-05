50b9e27e4ec6ca9681865bf5ab61dbce00eac3c3
Joanda Febrian <febrianjoanda@gmail.com>
2021-11-05 21:56:43 +0700

    feat: Overview Component

--- genersa/Modules/Components/CircularProgressBar.swift
+++ genersa/Modules/Components/CircularProgressBar.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct CircularProgressBar<Content: View>: View {
     
-    @Binding var progress: Double
+    let bars: [Progress]
     let content: Content
     let size: CGFloat
     
-    init(size: CGFloat, progress: Binding<Double>, @ViewBuilder content: @escaping () -> Content) {
+    init(size: CGFloat, bars: [Progress], @ViewBuilder content: @escaping () -> Content) {
         self.size = size
-        self._progress = progress
+        self.bars = bars
         self.content = content()
     }
     
@@ -27,76 +27,72 @@ struct CircularProgressBar<Content: View>: View {
             Circle()
                 .stroke(lineWidth: size / 8)
                 .foregroundColor(Color.gray)
-            Circle()
-                .trim(from: 0.0, to: CGFloat(min(self.progress, 1.0)))
-                .stroke(style: StrokeStyle(lineWidth: size / 8, lineCap: .round, lineJoin: .round))
-                .foregroundColor(Color.black)
-                .rotationEffect(Angle(degrees: 270.0))
-                .animation(.linear)
+            ForEach(bars, id: \.color) { bar in
+                Circle()
+                    .trim(from: 0.0, to: CGFloat(min(bar.progress, 1.0)))
+                    .stroke(style: StrokeStyle(lineWidth: size / 8, lineCap: .round, lineJoin: .round))
+                    .foregroundColor(bar.color)
+                    .rotationEffect(Angle(degrees: 270.0))
+                    .animation(.linear)
+            }
         }
         .frame(width: size + (size / 8), height: size + (size / 8), alignment: .center)
         .padding(8)
     }
 }
 
-struct CircularProgressPreview: View {
+struct Progress {
+    let progress: Double
+    let color: Color
+}
+
+struct CircularProgressPreview<Content: View>: View {
     
-    @State var progress: Double
+    let bars: [Progress]
+    let content: Content
+    let size: CGFloat
+    
+    init(size: CGFloat, bars: [Progress], @ViewBuilder content: @escaping () -> Content) {
+        self.bars = bars
+        self.size = size
+        self.content = content()
+    }
     
     var body: some View {
-        VStack {
-            Button {
-                withAnimation {
-                    progress = Double.random(in: 0...1)
-                }
-            } label: {
-                CircularProgressBar(size: 48, progress: $progress) {
-                    VStack {
-                        Text(String(format: "%.0f%%", progress*100.0))
-                            .font(.caption)
-                    }
-                }
-                .contentShape(Circle())
-            }
-            .buttonStyle(.plain)
-            
-            Button {
-                withAnimation {
-                    progress = Double.random(in: 0...1)
-                }
-            } label: {
-                CircularProgressBar(size: 48, progress: $progress) {
-                    VStack {
-                        BudgetIcon(color: Color.red, icon: Image(systemName: "car.fill"), iconSize: 48)
-                    }
-                }
-                .contentShape(Circle())
-            }
-            .buttonStyle(.plain)
-            
-            Button {
-                withAnimation {
-                    progress = Double.random(in: 0...1)
-                }
-            } label: {
-                CircularProgressBar(size: 144, progress: $progress) {
-                    VStack {
-                        Text("Current Balance")
-                            .foregroundColor(.secondary)
-                        Text("Rp1.020.000")
-                            .bold()
-                            .foregroundColor(.primary)
-                    }
-                }
-                .contentShape(Circle())
-            }
-            .buttonStyle(.plain)
+        CircularProgressBar(size: size, bars: bars) {
+            content
         }
     }
 }
 
 struct CircularProgressBar_Previews: PreviewProvider {
     static var previews: some View {
-        CircularProgressPreview(progress: 0.8)
+        VStack {
+            CircularProgressPreview(size: 48, bars: [
+                Progress(progress: 0.8, color: Color.red)
+            ]) {
+                VStack {
+                    Text(String(format: "%.0f%%", 80))
+                        .font(.caption)
+                }
+            }
+            CircularProgressPreview(size: 48, bars: [
+                Progress(progress: 0.8, color: Color.red)
+            ]) {
+                BudgetIcon(color: Color.green, icon: Image(systemName: "car.fill"), iconSize: 48)
+            }
+            CircularProgressPreview(size: 144, bars: [
+                Progress(progress: 0.8, color: Color.red),
+                Progress(progress: 0.3, color: Color.green)
+            ]) {
+                VStack {
+                    Text("Current Balance")
+                        .foregroundColor(.secondary)
+                    Text("Rp1.020.000")
+                        .bold()
+                        .foregroundColor(.primary)
+                }
+            }
+        }
     }
 }
--- genersa/Modules/Components/Overview.swift
+++ genersa/Modules/Components/Overview.swift
@@ -11,33 +11,26 @@ struct Overview: View {
     
     @ObservedObject private var viewModel: OverviewViewModel
     
-    init(totalUsed: Double, totalSaved: Double, needToSave: Double) {
-        self.viewModel = OverviewViewModel(totalUsed: totalUsed, totalSaved: totalSaved, needToSave: needToSave)
+    init(totalUsed: Double, totalSaved: Double, totalBudget: Double) {
+        self.viewModel = OverviewViewModel(totalUsed: totalUsed, totalSaved: totalSaved, totalBudget: totalBudget)
     }
     
     var body: some View {
         HStack(spacing: 16) {
-            Circle()
-                .frame(width: 144, height: 144)
-            OverviewLegends(totalUsed: viewModel.totalUsed, totalSaved: viewModel.totalSaved, needToSave: viewModel.needToSave)
+            CircularProgressBar(size: 144, bars: viewModel.progresses) {
+                VStack {
+                    Text("Current Balance")
+                        .foregroundColor(.secondary)
+                    Text(viewModel.currentBalance)
+                        .bold()
+                        .foregroundColor(.primary)
+                }
+            }
+            OverviewLegends(totalUsed: viewModel.totalUsed, totalSaved: viewModel.totalSaved, needToSave: viewModel.totalBudget - viewModel.totalSaved)
         }
-        .padding(32)
-        .frame(height: 200)
+        .padding(16)
         .background(RoundedRectangle(cornerRadius: 20)
                         .foregroundColor(.gray.opacity(0.3)))
-    }
-}
-
-final class OverviewViewModel: ObservableObject {
-    
-    @Published var totalUsed: Double
-    @Published var totalSaved: Double
-    @Published var needToSave: Double
-    
-    init(totalUsed: Double, totalSaved: Double, needToSave: Double) {
-        self.totalUsed = totalUsed
-        self.totalSaved = totalSaved
-        self.needToSave = needToSave
     }
 }
 
@@ -48,10 +41,10 @@ struct OverviewLegends: View {
     let needToSave: Double
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 16) {
             LegendView(name: "Total Used", amount: totalUsed, color: .blue)
             LegendView(name: "Total Saved", amount: totalSaved, color: .green)
-            LegendView(name: "Need to Save", amount: needToSave, color: .red)
+            LegendView(name: "Need to Save", amount: needToSave, color: .gray)
         }
     }
 }
@@ -70,10 +63,10 @@ struct LegendView: View {
     }
     
     var body: some View {
-        HStack(alignment: .top) {
+        HStack(alignment: .top, spacing: 4) {
             Circle()
                 .frame(width: 12, height: 12)
-                .padding(2)
+                .padding(4)
                 .foregroundColor(color)
             VStack(alignment: .leading) {
                 Text(name)
@@ -87,7 +80,7 @@ struct LegendView: View {
 
 struct Overview_Previews: PreviewProvider {
     static var previews: some View {
-        Overview(totalUsed: 0, totalSaved: 0, needToSave: 5000000)
+        Overview(totalUsed: 250000, totalSaved: 4000000, totalBudget: 5000000)
             .padding(16)
     }
 }
